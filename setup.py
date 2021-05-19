102269ecbf6bc09a315bd0963d60dc4ec9c512fe
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-05-19 16:38:41 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Uncertainty cascade
+
+"""
+
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+requirements = list(
+    filter(
+        lambda i: "git://" not in i,
+        Path("requirements.txt").read_text().strip().split("\n"),
+    )
+)
+
+setup(
+    name="errapids",
+    version="0.1.dev0",
+    description="Propagate uncertainty across models",
+    packages=find_packages(exclude=["doc", "testing", "tests", "data", "expt", "tmp"]),
+    install_requires=requirements,
+)
