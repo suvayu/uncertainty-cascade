11d0f5a81cca88378cd8b418fadef687e7f409cb
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-05-26 20:17:05 +0200

    ons.py: helper to merge dictionaries (copied from typedconfig)

--- errapids/ons.py
+++ errapids/ons.py
@@ -1,6 +1,7 @@
 """Overrides and scenarios"""
 
-from itertools import product
+from collections import Counter
+from itertools import product, chain
 from pathlib import Path
 from typing import Dict
 
@@ -8,6 +9,61 @@ from glom import glom, Assign
 
 
 def get_demand_profiles(config: Dict, demand_profile_dir: str, output: str) -> Dict:
+def merge_dicts(confs: List[Dict]) -> Dict:
+    """Merge a sequence of dictionaries
+
+    Common keys at the same depth are recursively reconciled.  The newer value
+    overwrites earlier values.  The order of the keys are preserved.  When
+    merging repeated keys, the position of the first occurence is considered as
+    correct.
+
+    Parameters
+    ----------
+    confs: Sequence[Dict]
+        A list of dictionaries
+
+    Returns
+    -------
+    Dict
+        Merged dictionary
+
+    Examples
+    --------
+
+    - e & b.d tests overwriting values
+    - b, e & b.d tests key ordering
+    - e & e.* tests adding new sub-keys
+
+    >>> d1 = {"a": 1, "b": {"c": 3, "d": 4}, "e": True}
+    >>> d2 = {"c": 3, "b": {"e": 5, "d": 40}, "e": {"g": True, "h": "foo"}}
+    >>> expected = {
+    ...     "a": 1,
+    ...     "b": {"c": 3, "d": 40, "e": 5},
+    ...     "e": {"g": True, "h": "foo"},
+    ...     "c": 3,
+    ... }
+    >>> result = merge_dicts([d1, d2])
+    >>> result == expected
+    True
+    >>> list(result) == list(expected)  # key ordering preserved
+    True
+    >>> list(result["b"]) == list(expected["b"])  # key ordering preserved
+    True
+
+    """
+    if not all(map(lambda obj: isinstance(obj, dict), confs)):
+        return confs[-1]
+
+    res = {}
+    for key, count in Counter(chain.from_iterable(confs)).items():
+        matches = [conf[key] for conf in confs if key in conf]
+        if count > 1:
+            res[key] = merge_dicts(matches)  # duplicate keys, recurse
+        else:
+            res[key] = matches[0]  # only one element
+    return res
+
+
     _output = Path(output)
     return glom(
         {},
