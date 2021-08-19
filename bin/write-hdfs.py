00c160a43fd5d91d211571e7ff22c52062d788f8
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-08-19 17:02:03 +0200

    write-hdfs.py: add all deltas

--- bin/write-hdfs.py
+++ bin/write-hdfs.py
@@ -3,12 +3,13 @@
 from argparse import ArgumentParser
 
 from errapids.err import scenario_deltas
-from errapids.metrics import ScenarioGroups
+from errapids.metrics import ScenarioGroups, pan_eu_cf, pan_eu_prod_share
 
 
 parser = ArgumentParser()
 parser.add_argument("-o", "--output")
 parser.add_argument("-i", "--input")
+
 
 if __name__ == "__main__":
     opts = parser.parse_args()
@@ -20,3 +21,14 @@ if __name__ == "__main__":
         arr.to_hdf(opts.output, f"metrics/{arr.name}")
         delta = scenario_deltas(arr)
         delta.to_hdf(opts.output, f"deltas/{arr.name}")
+
+    for grpby in ("region", "technology"):
+        delta = pan_eu_cf(scgrp["carrier_prod"], scgrp["energy_cap"], grpby)
+        delta.to_hdf(opts.output, f"deltas/{delta.columns[0]}")
+
+    delta = pan_eu_prod_share(scgrp["carrier_prod"])
+    delta.to_hdf(opts.output, f"deltas/{delta.columns[0]}")
+
+    for metric in scgrp.varnames_ts:
+        ts = scgrp.get(metric, False)
+        ts.to_hdf(opts.output, f"ts/{metric}_ts")
