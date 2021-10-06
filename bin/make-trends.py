9abac96956eb62b4de7e4665ce7a0daae7f36a34
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-10-06 14:48:39 +0200

    make-trends.py: update time series script

--- bin/make-trends.py
+++ bin/make-trends.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-from collections import defaultdict
-from itertools import product
 from pathlib import Path
 import sys
 
 import holoviews as hv
-from tqdm import tqdm
 
-from errapids.err import baselines, demand_lvls
 from errapids.io import HDF5Reader
 from errapids.viz import trendmanager
 
@@ -16,6 +12,7 @@ hv.extension("bokeh")
 parser = ArgumentParser()
 parser.add_argument("-i", "--input", help="HDF5 file with dataframes")
 parser.add_argument("-o", "--output", help="plot directory")
+
 
 if __name__ == "__main__":
     opts = parser.parse_args()
@@ -32,26 +29,7 @@ if __name__ == "__main__":
     con = reader.ts("carrier_con").droplevel("carrier")
     prod = reader.ts("carrier_prod").droplevel("carrier")
     countries = "GBR DEU ESP ITA FRA PRT DNK NOR".split()
-
-    pbar = tqdm(demand_lvls)
-    plots = defaultdict(list)
     mgr = trendmanager(con, prod, within=countries)
-    for val in pbar:
-        pbar.set_description(f"EV={val}")
-        lvls = [
-            val if l == "EV" else v
-            for l, v in baselines.items()
-            if not isinstance(v, list)
-        ]
-        mgr.lvls = lvls
-        for region, export in product(countries, (True, False)):
-            key = (region, export)
-            plot = mgr.plot(region, export=export)
-            plots[key].append(
-                plot.opts(title=plot.opts.get().kwargs["title"] + f"(EV={val})")
-            )
-    for (region, export), plot in plots.items():
-        hv.save(
-            hv.Layout(plot).cols(1),
-            f"{plotdir}/electricity_{region}_{mgr.legend(export)}_EV.html",
-        )
+
+    for lvl in ("EV", "heating"):
+        mgr.write(lvl, plotdir)
